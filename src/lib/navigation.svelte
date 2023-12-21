c4d15139975803496cae3182e039711cf5082a0a
Greyson Richey <greysonrichey@gmail.com>
2023-12-21 03:00:56 -0700

    Add Skeleton ui library
    - Lots of project config
    - Some basic structure for app

---
+++ src/lib/navigation.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+  import { getDrawerStore } from '@skeletonlabs/skeleton';
+  import { page } from '$app/stores';
+
+  const drawerStore = getDrawerStore();
+
+  function drawerClose(): void {
+    drawerStore.close();
+  }
+  console.log($page.url.pathname);
+</script>
+
+<nav class="list-nav p-4">
+  <ul>
+    <li>
+      <a class="anchor" class:active={$page.url.pathname === '/'} href="/" on:click={drawerClose}>
+        Dashboard
+      </a>
+    </li>
+    <li>
+      <a
+        class="anchor"
+        class:active={$page.url.pathname === '/pieces'}
+        href="/pieces"
+        on:click={drawerClose}
+      >
+        Pieces
+      </a>
+    </li>
+    <li>
+      <a
+        class="anchor"
+        class:active={$page.url.pathname === '/todo'}
+        href="/todo"
+        on:click={drawerClose}
+      >
+        Todo
+      </a>
+    </li>
+    <li>
+      <a
+        class="anchor"
+        class:active={$page.url.pathname === '/test-tiles'}
+        href="/test-tiles"
+        on:click={drawerClose}
+      >
+        Test Tiles
+      </a>
+    </li>
+    <li>
+      <a
+        class="anchor"
+        class:active={$page.url.pathname === '/stages'}
+        href="/stages"
+        on:click={drawerClose}
+      >
+        Stages
+      </a>
+    </li>
+  </ul>
+</nav>
+
+<style>
+  .active {
+    background-color: rgb(var(--color-primary-500) / 0.1) !important;
+  }
+</style>
