bb497d973dc2329955366b51a92e1d82f0d8b59a
Bloodyaugust <greysonrichey@gmail.com>
2024-05-19 19:38:27 -0600

    Add the ability to remove types

--- src/routes/types/+page.svelte
+++ src/routes/types/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
   import { dexie } from '$lib/stores/dexie';
-  import { getToastStore } from '@skeletonlabs/skeleton';
+  import type { PieceType } from '$lib/types/pieceType';
+  import { getModalStore, getToastStore, type ModalSettings } from '@skeletonlabs/skeleton';
   import { liveQuery } from 'dexie';
 
   const toastStore = getToastStore();
+  const modalStore = getModalStore();
 
   let dbTypes = liveQuery(() => dexie.types.toCollection().sortBy('sortOrder'));
   let newType: string = '';
@@ -21,6 +23,25 @@
       message: 'Type added!',
     });
   }
+
+  async function handleRemoveTypeClick(type: PieceType) {
+    new Promise<boolean>((resolve) => {
+      const confirmModal: ModalSettings = {
+        type: 'confirm',
+        title: 'Confirm Type Removal',
+        body: `Are you sure you want to remove the ${type.name} type?`,
+        response: (r: boolean) => {
+          resolve(r);
+        },
+      };
+
+      modalStore.trigger(confirmModal);
+    }).then((confirmed) => {
+      if (confirmed) {
+        dexie.types.delete(type.id);
+      }
+    });
+  }
 </script>
 
 <div class="flex flex-col gap-4">
@@ -35,12 +56,29 @@
         bind:value={newType}
       />
     </div>
-    <button type="button" class="variant-filled btn" on:click={handleAddTypeClick}>Add Type</button>
+    <button
+      type="button"
+      class="variant-filled btn"
+      on:click={handleAddTypeClick}
+      disabled={newType === ''}
+    >
+      Add Type
+    </button>
   </div>
   <hr />
   {#if $dbTypes?.length}
     {#each $dbTypes as type (type.id)}
-      <span class="h3">{type.name}</span>
+      <div class="flex">
+        <span class="h3 p-4">{type.name}</span>
+        <button
+          class="variant-filled-warning btn"
+          on:click={() => {
+            handleRemoveTypeClick(type);
+          }}
+        >
+          Remove Type
+        </button>
+      </div>
     {/each}
   {/if}
 </div>
